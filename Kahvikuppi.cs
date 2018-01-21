39aed07e5809f2a4787b75e336045f6aec99d87a
MJarvila <35224611+MJarvila@users.noreply.github.com>
2018-01-21 20:29:25 +0200

    Add files via upload

---
+++ Kahvikuppi.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Har3Teht6
+{
+    class Coffeecup
+    {
+        private int MaxCoffee = 100;
+
+        public int coffee;
+        public int Coffee
+        {
+            get { return coffee; }
+            set
+            {
+                if (value <= MaxCoffee)
+                    coffee = value;
+                else
+                {
+                    coffee = MaxCoffee;
+                    Console.WriteLine("You poured too much. The cup is filled to the brim and some spilled over.");
+                }
+            }
+        }
+
+        public string Color { get; set; }
+        public bool Decoration { get; set; }
+        public bool Handle { get; set; }
+
+
+
+        public Coffeecup(string color, bool decoration, bool handle)
+        {
+            Color = color;
+            Decoration = decoration;
+            Handle = handle;
+            Coffee = 0;
+        }
+        public void Pour()
+        {
+            Coffee += 25;
+            Console.WriteLine("You poured some.");
+        }
+
+        public void Drink()
+        {
+            Coffee -= 25;
+            Console.WriteLine("You drank some.");
+        }
+
+        public void LookAtCup()
+        {
+            Console.WriteLine("Your cup is: ");
+            Console.WriteLine(Coffee + "% full");
+            Console.WriteLine("And it's " + Color);
+            if (Handle == true)
+                Console.WriteLine("It seems to be decorated.");
+            else
+                Console.WriteLine("It's just a plain, " + Color + " cup.");
+            if (Decoration == true)
+                Console.WriteLine("It has a handle.");
+            else
+                Console.WriteLine("It doesn't have a handle.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// coffee, color, decoration, handle
+
+namespace Har3Teht6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice;
+            Coffeecup first = new Coffeecup("blue", true, true);
+            first.Coffee = 95;
+
+            first.LookAtCup();
+            Console.ReadLine();
+            first.Drink();
+            first.Drink();
+            first.Drink();
+            first.Drink();
+            first.Pour();
+            first.LookAtCup();
+        }
+    }
+}
