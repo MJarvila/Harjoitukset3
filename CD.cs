d351eb26cb2dce23300318c21c9f994c9f690510
MJarvila <35224611+MJarvila@users.noreply.github.com>
2018-01-21 20:30:03 +0200

    Add files via upload

---
+++ CD.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Har3Teht7
+{
+    class CD
+    {
+        public string Artist { get; set; }
+
+        public string AlbumName { get; set; }
+
+        public int Year { get; set; }
+
+        public float TotalLenght { get; set; }
+
+        public string Genre { get; set; }
+
+        public double Price { get; set; }
+
+        public string Tracklist { get; set; }
+
+        public CD(string artist, string albumName, int year, float totalLenght, string genre, double price, string tracklist)
+        {
+            Artist = artist;
+            AlbumName = albumName;
+            Year = year;
+            TotalLenght = totalLenght;
+            Genre = genre;
+            Price = price;
+            Tracklist = tracklist;
+        }
+        public void LookAtCd()
+        {
+            Console.WriteLine("Artist - " + Artist);
+            Console.WriteLine("Album - " + AlbumName);
+            Console.WriteLine("Year - " + Year);
+            Console.WriteLine("Lenght - " + TotalLenght + " min");
+            Console.WriteLine("Genre - " + Genre);
+            Console.WriteLine("Price - " + Price + " $");
+            Console.WriteLine("Songs: " + Tracklist);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,26 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-// coffee, color, decoration, handle
+// Artist, AlbumName, Year, TotalLenght, Genre, Price, TrackList
 
-namespace Har3Teht6
+namespace Har3Teht7
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int choice;
-            Coffeecup first = new Coffeecup("blue", true, true);
-            first.Coffee = 95;
-
-            first.LookAtCup();
-            Console.ReadLine();
-            first.Drink();
-            first.Drink();
-            first.Drink();
-            first.Drink();
-            first.Pour();
-            first.LookAtCup();
+            CD joku = new CD("Jokuset", "Jaskattelua", 1990, 68, "Nu Metal", 200.2, "\n--- 1. Jaska Jokunen 12:19 \n--- 2. Comeback 5:23 \n--- 3. Vaikkei missään oltukaan 6:66 \n--- 4. Kökkö loppubiisi 5:43 \n--- 5. Loppusuhinaa kun ei muutakaan keksitty bändin kanssa (feat. Masan Pieru) 37:29");
+            joku.LookAtCd();
         }
     }
 }
